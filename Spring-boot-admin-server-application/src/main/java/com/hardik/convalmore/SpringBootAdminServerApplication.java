9c6dbdf1ae91e7ad0e7193c34f9626c107fa48bf
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2021-07-28 16:40:49 +0530

    init server application commit

---
+++ Spring-boot-admin-server-application/src/main/java/com/hardik/convalmore/SpringBootAdminServerApplication.java
@@ -0,0 +1,13 @@
+package com.hardik.convalmore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminServerApplication.class, args);
+	}
+
+}
